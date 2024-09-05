32f3b0046e6d3950fb8cffc83b5169e682232f3a
lee <lsljw65@gmail.com>
2024-09-05 12:23:12 +0900

    style , script, comment 작성

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// javascript와 jQuery의 기술방식에 차이가 있습니다.
+/* javascript */
 function external() {
   alert("External Hello Word");
 }
+// jQuery
+$(function () {
+  $("#button1").click(function () {
+    alert("External Hello Word");
+  });
+});
